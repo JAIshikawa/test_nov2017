e211392706ecbf6f05458c0883bcee944ad6e634
Julie-Ann Ishikawa <JulieAnn.Ishikawa@gov.bc.ca>
2017-12-05 14:19:36 -0800

    add summary of empty cars

--- cars.R
+++ cars.R
@@ -3,3 +3,10 @@ library(ggplot2)
 
 date("mtcars")
 glimpse(mtcars)
+
+mt_cars_summary <- mtcars %>% 
+  group_by(cyl) %>% 
+  summarise(mean_disp =mean(disp), 
+            mean_hp = mean(hp))
+
+mt_cars_summary
